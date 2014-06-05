21b24ab27b521cc19746a6169d28793401c7c59b
Aparna Jain <aparna@Admins-MacBook-Pro-2.local>
2014-06-04 20:15:25 -0700

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Aparna Jain on 6/4/14.
+//  Copyright (c) 2014 FOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
